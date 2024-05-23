2a750a63e20f867c69221867652391b1e41447a2
skatj351 <skatj351@gmail.com>
2024-05-23 19:01:19 +0900

    no message

--- nsh/presentation_model.dart
+++ nsh/presentation_model.dart
@@ -1,8 +1,4 @@
-import '/flutter_flow/flutter_flow_icon_button.dart';
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
-import 'input_widget.dart' show InputWidget;
+import 'package:flutterflow_ui/flutterflow_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
